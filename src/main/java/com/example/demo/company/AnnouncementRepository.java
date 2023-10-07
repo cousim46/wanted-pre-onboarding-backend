ddd74dd38b57720d6c5796e6cadfa8c0d28b8a1c
cousim46 <wjdghldns24@naver.com>
2023-10-08 02:17:45 +0900

    Add : 공고와 관련된 repository 추가

---
+++ src/main/java/com/example/demo/company/AnnouncementRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.company;
+
+/**
+ * packageName    : com.example.demo.company
+ * fileName       : AnnouncementRepository
+ * author         : jhw1015
+ * date           : 2023/10/08
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/10/08           jhw1015           최초 생성
+ */
+public class AnnouncementRepository {
+
+}
