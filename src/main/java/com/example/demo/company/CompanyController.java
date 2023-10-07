e384fd3159b59f59e61947fd641e27fe9fb08720
cousim46 <wjdghldns24@naver.com>
2023-10-08 02:16:25 +0900

    Add : 회사에 대한 컨트롤러 추가 - 회사 등록 API 추가

---
+++ src/main/java/com/example/demo/company/CompanyController.java
@@ -0,0 +1,21 @@
+package com.example.demo.company;
+
+import com.example.demo.company.dto.CompanyDto.Create;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/company")
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final CompanyWriteService companyWriteService;
+    @PostMapping("/create")
+    public Long create(@Valid @RequestBody Create create) {
+        return companyWriteService.create(create.getName(),create.getCountry(),create.getName());
+    }
+}
