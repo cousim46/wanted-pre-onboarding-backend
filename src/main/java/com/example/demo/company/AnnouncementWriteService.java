9b13ba64896057e2c5101df780b4d35ee8c8e82a
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:25:45 +0900

    Add : 채용공고 신청 및 수정 비즈니스 로직

--- src/main/java/com/example/demo/company/AnnouncementWriteService.java
+++ src/main/java/com/example/demo/company/AnnouncementWriteService.java
@@ -1,10 +1,15 @@
 package com.example.demo.company;
 
+import com.example.demo.account.AccountRepository;
+import com.example.demo.account.domain.Account;
 import com.example.demo.common.error.ErrorCode;
 import com.example.demo.common.error.GlobalRuntimeException;
 import com.example.demo.company.domain.Announcement;
+import com.example.demo.company.domain.AnnouncementHistory;
 import com.example.demo.company.domain.Company;
 import com.example.demo.company.dto.AnnouncementDto.CreateRequest;
+import com.example.demo.company.dto.AnnouncementDto.RegisterRequest;
+import com.example.demo.company.dto.AnnouncementDto.UpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +19,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class AnnouncementWriteService {
+
     private final AnnouncementRepository announcementRepository;
     private final CompanyRepository companyRepository;
+    private final AccountRepository accountRepository;
+    private final AnnouncementHistoryRepository announcementHistoryRepository;
 
-    public Long create(CreateRequest request)  {
+    public Long create(CreateRequest request) {
         Company company = companyRepository.findById(request.getCompanyId())
             .orElseThrow(() -> new GlobalRuntimeException(
                 ErrorCode.NOT_FOUND_COMPANY));
@@ -28,5 +36,24 @@ public class AnnouncementWriteService {
 
     public void delete(Long id) {
         announcementRepository.deleteById(id);
+    }
+
+    public void update(Long id, UpdateRequest updateRequest) {
+        Announcement announcement = announcementRepository.findById(id)
+            .orElseThrow(() -> new GlobalRuntimeException(ErrorCode.NOT_FOUND_ANNOUNCEMENT));
+
+        announcement.update(updateRequest.getPosition(), updateRequest.getContent(),
+            updateRequest.getSkill(), updateRequest.getRecruitmentCompensation());
+    }
+
+    public Long register(RegisterRequest request) {
+        Account account = accountRepository.findById(request.getAccountId())
+            .orElseThrow(() -> new GlobalRuntimeException(ErrorCode.NOT_FOUND_ACCOUNT));
+        Announcement announcement = announcementRepository.findById(request.getAnnouncementId())
+            .orElseThrow(() -> new GlobalRuntimeException(ErrorCode.NOT_FOUND_ANNOUNCEMENT));
+        if(announcementHistoryRepository.existsByAccountAndAnnouncement(account, announcement)) {
+            throw new GlobalRuntimeException(ErrorCode.ALREADY_REGISTER);
+        }
+        return announcementHistoryRepository.save(new AnnouncementHistory(account,announcement)).getId();
     }
 }
