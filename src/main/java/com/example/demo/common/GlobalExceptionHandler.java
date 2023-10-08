7421750bb496f69027e0c5b5e0f5855206f85866
cousim46 <wjdghldns24@naver.com>
2023-10-08 20:42:33 +0900

    Add : CustomException에 대한 처리

--- src/main/java/com/example/demo/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.example.demo.common;
 
+import com.example.demo.common.error.ErrorCode;
+import com.example.demo.common.error.GlobalRuntimeException;
+import com.example.demo.common.error.RuntimeExceptionResponse;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +22,12 @@ public class GlobalExceptionHandler {
             response.put(((FieldError) error).getField(), error.getDefaultMessage())
             );
         return response;
+    }
+
+    @ExceptionHandler({GlobalRuntimeException.class})
+    public static ResponseEntity<RuntimeExceptionResponse> response(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getStatus())
+            .body(RuntimeExceptionResponse.create(errorCode));
     }
 }
 
