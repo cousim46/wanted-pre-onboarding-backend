51fb4162d460fcae91d016b1db0d173127a21ae4
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:27:48 +0900

    Add : 채용공고 수정, 검색, 신청에 대한 API

--- src/main/java/com/example/demo/company/AnnouncementController.java
+++ src/main/java/com/example/demo/company/AnnouncementController.java
@@ -2,12 +2,17 @@ package com.example.demo.company;
 
 import com.example.demo.company.dto.AnnouncementDto;
 import com.example.demo.company.dto.AnnouncementDto.DetailResponse;
+import com.example.demo.company.dto.AnnouncementDto.ListResponse;
+import com.example.demo.company.dto.AnnouncementDto.RegisterRequest;
+import com.example.demo.company.dto.AnnouncementDto.UpdateRequest;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +37,20 @@ public class AnnouncementController {
     @GetMapping("/{announcementId}")
     public DetailResponse detail(@PathVariable("announcementId") Long id) {
         return announcementReadService.detail(id);
+    }
+
+    @PutMapping("/{announcementId}")
+    public void update(@PathVariable("announcementId") Long id, @Valid @RequestBody UpdateRequest updateRequest) {
+        announcementWriteService.update(id,updateRequest);
+    }
+
+    @PostMapping("/register")
+    public Long register(@Valid @RequestBody RegisterRequest request) {
+        return announcementWriteService.register(request);
+    }
+
+    @GetMapping
+    public List<ListResponse> getAnnouncements(String search) {
+        return announcementReadService.getAnnouncements(search);
     }
 }
