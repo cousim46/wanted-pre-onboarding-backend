c1629be7a3d6c4ac060043ee4342ec836e94fc2a
cousim46 <wjdghldns24@naver.com>
2023-10-08 20:38:39 +0900

    Add : 채용공고 등록 API

---
+++ src/main/java/com/example/demo/company/AnnouncementController.java
@@ -0,0 +1,22 @@
+package com.example.demo.company;
+
+import com.example.demo.company.dto.AnnouncementDto;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/announcement")
+public class AnnouncementController {
+    private final AnnouncementWriteService announcementWriteService;
+
+    @PostMapping("/create")
+    public Long create(@Valid @RequestBody AnnouncementDto.CreateRequest request) {
+        return announcementWriteService.create(request);
+    }
+}
