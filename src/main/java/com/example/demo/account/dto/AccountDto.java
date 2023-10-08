9ed9030b66bedd54d6c568c0b321c90e43172682
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:32:16 +0900

    Delete : 불필요한 주석 및 import문 삭제

--- src/main/java/com/example/demo/account/dto/AccountDto.java
+++ src/main/java/com/example/demo/account/dto/AccountDto.java
@@ -1,21 +1,10 @@
 package com.example.demo.account.dto;
 
-import com.sun.istack.NotNull;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-/**
- * packageName    : com.example.demo.account.dto
- * fileName       : AccountDto
- * author         : jhw1015
- * date           : 2023/10/08
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023/10/08           jhw1015           최초 생성
- */
+
 public class AccountDto {
 
     @Getter
