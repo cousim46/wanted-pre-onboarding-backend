ed794e9a2f8743d12a4d1ad366d02d7cc680dc24
cousim46 <wjdghldns24@naver.com>
2023-10-08 20:44:21 +0900

    Add : 채용공고 상세보기, 생성에 대한 Dto

---
+++ src/main/java/com/example/demo/company/dto/AnnouncementDto.java
@@ -0,0 +1,50 @@
+package com.example.demo.company.dto;
+
+import com.example.demo.company.domain.Announcement;
+import com.example.demo.company.domain.Company;
+import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+public class AnnouncementDto {
+    @AllArgsConstructor
+    @Getter
+    public static class CreateRequest {
+        @NotNull
+        private Long companyId;
+        @NotBlank
+        private String position;
+        @NotBlank
+        private String content;
+        private long recruitmentCompensation;
+        @NotBlank
+        private String skill;
+    }
+
+    @Getter
+    public static class DetailResponse {
+        private final Long announcementId;
+        private final String companyName;
+        private final String country;
+        private final String area;
+        private final long recruitmentCompensation;
+        private final String skill;
+        private final String content;
+        private List<Long> announcementIds;
+
+        public DetailResponse(Announcement announcement, Company company, List<Long> announcementIds) {
+            this.announcementId = announcement.getId();
+            this.companyName = company.getName();
+            this.country = company.getCountry();
+            this.area = company.getArea();
+            this.recruitmentCompensation = announcement.getRecruitmentCompensation();
+            this.skill = announcement.getSkill();
+            this.content = announcement.getContent();
+            this.announcementIds = announcementIds;
+        }
+    }
+
+}
