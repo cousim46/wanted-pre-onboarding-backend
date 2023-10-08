f44a0a0686362244f1c1124a6b8735a91de24c9c
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:26:03 +0900

    Fix : 회사와 관련된 채용공고에 대한 메서드 쿼리 수정

--- src/main/java/com/example/demo/company/AnnouncementRepository.java
+++ src/main/java/com/example/demo/company/AnnouncementRepository.java
@@ -4,9 +4,11 @@ import com.example.demo.company.domain.Announcement;
 import com.example.demo.company.domain.Company;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
-    List<Long> findAllByCompany(Company company);
-
+    @Query("SELECT a.id From Announcement a WHERE a.company = :company")
+    List<Long> findAllByCompany(@Param("company") Company company);
 }
