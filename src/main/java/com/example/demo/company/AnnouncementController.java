0359254c9c8d0910ec925fc1feb63500a6857c29
cousim46 <wjdghldns24@naver.com>
2023-10-08 20:40:17 +0900

    Add : 채용공고 상세보기 API

--- src/main/java/com/example/demo/company/AnnouncementController.java
+++ src/main/java/com/example/demo/company/AnnouncementController.java
@@ -1,9 +1,12 @@
 package com.example.demo.company;
 
 import com.example.demo.company.dto.AnnouncementDto;
+import com.example.demo.company.dto.AnnouncementDto.DetailResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/announcement")
 public class AnnouncementController {
     private final AnnouncementWriteService announcementWriteService;
+    private final AnnouncementReadService announcementReadService;
 
     @PostMapping("/create")
     public Long create(@Valid @RequestBody AnnouncementDto.CreateRequest request) {
@@ -24,5 +28,9 @@ public class AnnouncementController {
     @DeleteMapping
     public void delete(Long announcementId) {
         announcementWriteService.delete(announcementId);
+    }
+    @GetMapping("/{announcementId}")
+    public DetailResponse detail(@PathVariable("announcementId") Long id) {
+        return announcementReadService.detail(id);
     }
 }
