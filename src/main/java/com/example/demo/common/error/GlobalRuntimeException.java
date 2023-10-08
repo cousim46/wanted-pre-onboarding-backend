5d351a00efa2c389da1e3efdfcbf19d4cb4f95f8
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:25:09 +0900

    Add : get메서드 추가

--- src/main/java/com/example/demo/common/error/GlobalRuntimeException.java
+++ src/main/java/com/example/demo/common/error/GlobalRuntimeException.java
@@ -2,10 +2,15 @@ package com.example.demo.common.error;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 
 @RequiredArgsConstructor
 @Getter
 public class GlobalRuntimeException extends RuntimeException{
     private final ErrorCode errorCode;
+
+    public HttpStatus getStatus() {
+        return errorCode.getStatus();
+    }
 }
