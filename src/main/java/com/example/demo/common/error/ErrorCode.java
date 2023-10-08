5f408bd349828108db39476610db5728b5daf54a
cousim46 <wjdghldns24@naver.com>
2023-10-08 23:24:34 +0900

    Add : 에러코드 추가

--- src/main/java/com/example/demo/common/error/ErrorCode.java
+++ src/main/java/com/example/demo/common/error/ErrorCode.java
@@ -4,23 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-/**
- * packageName    : com.example.demo.common.error
- * fileName       : ErrorCode
- * author         : jhw1015
- * date           : 2023/10/08
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023/10/08           jhw1015           최초 생성
- */
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    NOT_FOUND_ACCOUNT(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
     NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND,"회사가 존재하지 않습니다."),
-    NOT_FOUND_ANNOUNCEMENT(HttpStatus.NOT_FOUND,"채용공고가 존재하지 않습니다.");
+    NOT_FOUND_ANNOUNCEMENT(HttpStatus.NOT_FOUND,"채용공고가 존재하지 않습니다."),
+
+    ALREADY_REGISTER(HttpStatus.CONFLICT, "이미 신청한 채용 공고입니다.");
     private final HttpStatus status;
     private final String message;
 }
